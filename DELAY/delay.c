8ff2bda76672976765c3c7a4d385a6ef709afa68
LakshmiSwamy <clakshmiswami@gmail.com>
2026-06-22 18:28:28 +0530

    Create delay.c

---
+++ DELAY/delay.c
@@ -0,0 +1,17 @@
+void delay_us(unsigned int dlyUS)
+{
+	dlyUS*=12;
+	while(dlyUS--);
+}
+void delay_ms(unsigned int dlyMS)
+{
+	dlyMS*=12000;
+	while(dlyMS--);
+}
+
+void delay_s(unsigned int dlyS)
+{
+	dlyS*=12000000;
+	while(dlyS--);
+}
+
